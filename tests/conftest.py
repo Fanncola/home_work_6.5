3ce21e012f7f6750dea0b672a243bb693f847070
Aleksey Sazonov <sazonov1988@yandex.ru>
2023-07-12 10:08:26 +0500

    Рефакторинг тестов

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def browser_manager():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1360
-    browser.config.window_height = 768
+    browser.config.window_width = 1440
+    browser.config.window_height = 776
+
     yield
     browser.quit()
-
